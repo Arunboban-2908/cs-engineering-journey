27ad8f89875aedc352d07047a1b7bac41fe13a22
bon-can1 <arunboban66@gmail.com>
2026-09-04 11:18:43 +0530

    Initial commit - cleaned nested repo

---
+++ 01_DSA_And_Algorithms/01_Big_O/constant_time.py
@@ -0,0 +1,8 @@
+# Constant Time
+def constant_time(n):
+    return n * 2 + 1
+
+print(constant_time(10))
+
+# here we have a function that takes in a number and returns the number multiplied by 2 and then adds 1
+# this function is O(1) because it takes the same amount of time to run regardless of the size of the input
